f8a354d4600846288619de1ee6eda6abcb4c3e55
Matthew Pittendreigh <mattpitt1998@gmail.com>
2019-02-20 18:45:20 -0500

    More advanced example

---
+++ canvasAPIExample.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+token = "3~DDOooECkRUJNhiLUkbLC7YGVDsqKXnCyXMTHIXYuhRjMccphKa48bhWPgH6SaA3G"
+api = "https://keene.instructure.com/api/v1/"
+headers = {'Authorization' : 'Bearer ' + '%s' % token}
+
+url = api + 'courses?enrollment_state=active'
+
+results = requests.get(url, headers=headers)
+response = results.text
+
+values = json.loads(response)
+
+myCourse = input("Enter Course Name: ")
+
+for course in values:
+    if 'name' in course and course['name'] == myCourse:
+        assignments = requests.get('https://keene.instructure.com/api/v1/courses/' + str(course['id']) + '/assignments',
+                                    headers=headers)
+        jsonAssignments = json.loads(assignments.text)
+        for assignment in jsonAssignments:
+            if assignment['published']:
+                print(assignment['name'] + "=> " + str(assignment['due_at']))
+
+
+
